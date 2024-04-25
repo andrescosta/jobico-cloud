da7ff8916672a2eafff1358c0befc29839b346b8
Andres <andres.costa@gmail.com>
2024-04-25 16:38:09 +0000

    lib

---
+++ dh.sh
@@ -0,0 +1,3 @@
+dhcp_release virbr0 192.168.122.7 02:01:c3:26:63:bc
+dhcp_release virbr0 192.168.122.193 02:d5:eb:19:8c:da
+dhcp_release virbr0 192.168.122.241 02:f2:64:2d:d6:25
---
+++ scripts/l.sh
@@ -0,0 +1,1 @@
+sed /# B Kube/,/# E Kube/d /etc/hosts >> scripts/lib.sh 
