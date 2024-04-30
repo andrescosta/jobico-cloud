93517c26eb12898cd136803dce87f3911be3a8f2
Andres <andres.costa@gmail.com>
2024-04-30 16:19:53 +0000

    ha

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -11,3 +11,9 @@ escape() {
     escaped_result=$(printf '%s\n' "$1" | sed -e 's/[]\/$*.^[]/\\&/g')
     echo "${escaped_result}"
 }
+print_array_to_file(){
+    values=($@)
+    for v in "${values[@]}"; do
+        echo "$v" > array.txt
+    done
+}
