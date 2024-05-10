35b891532febf6a3538044eafcc5e3e12fca2c0e
Andres <andres.costa@gmail.com>
2024-05-10 19:00:15 +0000

    fix

---
+++ scripts/support/ssh.sh
@@ -0,0 +1,7 @@
+SCP() {
+    scp -o ConnectTimeout=30 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 "$@"
+}
+SSH(){
+    ssh -o ConnectTimeout=30 -o ServerAliveInterval=60 -o ServerAliveCountMax=3 "$@"
+}
+
