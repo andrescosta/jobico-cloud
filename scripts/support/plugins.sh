2e21e3227d4edab1638d5f64d14fa17cdbe2b8c1
Andres <andres.costa@gmail.com>
2024-05-13 15:26:15 +0000

    exec

--- scripts/support/plugins.sh
+++ scripts/support/plugins.sh
@@ -6,17 +6,20 @@ PLUGINS_DIR=${DIR}/scripts/plugins
 
 kube::plugins::load(){
     local plugins_conf=$1
-    while IFS='=' read -r func plugin || [[ -n "$func" ]]; do 
-        if [ -z "${plugin}" ]; then
-            continue
+    while IFS='=' read -r func plugin; do 
+        DEBUG echo "Plugin $plugin($func)"
+        if [ "${plugin}" == "" ]; then
+            throw "Illegal format: The plugin ${plugin} is empty."
+        fi
+        if [ "${func}" == "" ]; then
+            throw "The function ${func} is empty."
         fi
         plugin_file="${PLUGINS_DIR}/$plugin"
         DEBUG echo ">>Loading plugin: ${plugin_file}"
         if [ -f "${plugin_file}" ]; then
             source "$plugin_file"
         else
-            echo "The plugin ${plugin_file} was not found."
-            exit 1
+            throw "The plugin ${plugin_file} was not found."
         fi
     done < "${plugins_conf}"
 }
