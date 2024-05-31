8f253101e47c10e1fb490d7d79df9fa9188409e7
Andres <andres.costa@gmail.com>
2024-05-31 18:53:38 +0000

    op

--- scripts/plugins/networking.sh
+++ scripts/plugins/networking.sh
@@ -17,17 +17,21 @@ kube::net::add_routes(){
 EOF
         done
     done
-    kube::dao::cluster::all_nodes | while read IP1 FQDN1 HOST1 SUBNET1 TYPE1 SCH1; do
-         kube::dao::cluster::members | while read IP2 FQDN2 HOST2 SUBNET2 TYPE2 SCH2; do
+    s=$(kube::dao::cluster::count node)
+    if [[ $s > 0 ]]; then
+        kube::dao::cluster::all_nodes | while read IP1 FQDN1 HOST1 SUBNET1 TYPE1 SCH1; do
+          kube::dao::cluster::members | while read IP2 FQDN2 HOST2 SUBNET2 TYPE2 SCH2; do
             if [ "${IP1}" != "${IP2}" ]; then
+                echo "$IP1" >> ll
                 SSH root@${IP1} \
 <<EOF 
     sed -i "/set-name: enp1s0/a\            routes:\n              - to: ${SUBNET2}\n                via: ${IP2}" /etc/netplan/50-cloud-init.yaml 
     netplan apply
 EOF
             fi
+          done
         done
-    done
+    fi
 }
 kube::net::add_routes_to_added_node(){
     kube::dao::cluster::nodes | while read IP1 FQDN1 HOST1 SUBNET1 TYPE1 SCH1; do
