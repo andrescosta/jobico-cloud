bdedadcb394577419116f084849ef6f743ed1f3a
Andres <andres.costa@gmail.com>
2024-06-12 11:26:57 -0400

    refactors

--- scripts/controller.sh
+++ scripts/controller.sh
@@ -1,6 +1,6 @@
 jobico::init() {
     jobico::local::init_fs
-    if ! jobico::was_done "init"; then
+    if [ $(jobico::was_done "init") == false ]; then
         jobico::dao::gen_databases "$@"
         NOT_DRY_RUN jobico::local::download_deps
         NOT_DRY_RUN jobico::local::install_kubectl
@@ -9,13 +9,13 @@ jobico::init() {
 }
 jobico::create_cluster() {
     # Machines
-    if ! jobico::was_done "machines"; then
+    if [ $(jobico::was_done "machines") == false ]; then
         NOT_DRY_RUN jobico::vm::create
         NOT_DRY_RUN jobico::vm::wait_until_all_up
         jobico::set_done "machines"
     fi
     # DNS
-    if ! jobico::was_done "host"; then
+    if [ $(jobico::was_done "host") == false ]; then
         jobico::host::gen_hostsfile
         NOT_DRY_RUN jobico::host::update_local_etc_hosts
         NOT_DRY_RUN jobico::host::update_local_known_hosts
@@ -24,7 +24,7 @@ jobico::create_cluster() {
         jobico::set_done "host"
     fi
     # TLS
-    if ! jobico::was_done "tls_certs"; then
+    if [ $(jobico::was_done "tls_certs") == false ]; then
         jobico::tls::gen_ca_conf
         jobico::tls::gen_ca
         jobico::tls::gen_certs
@@ -33,13 +33,13 @@ jobico::create_cluster() {
         jobico::set_done "tls_certs"
     fi
     # HAProxy
-    if ! jobico::was_done "haproxy"; then
+    if [ $(jobico::was_done "haproxy") == false ]; then
         jobico::haproxy::gen_cfg
         NOT_DRY_RUN jobico::haproxy::deploy
         jobico::set_done "haproxy"
     fi
     # Kubeconfig
-    if ! jobico::was_done "kubeconfig"; then
+    if [ $(jobico::was_done "kubeconfig") == false ]; then
         jobico::kubeconfig::gen_for_nodes
         jobico::kubeconfig::gen_for_controlplane
         jobico::kubeconfig::gen_for_kube_admin
@@ -48,51 +48,51 @@ jobico::create_cluster() {
         jobico::set_done "kubeconfig"
     fi
     # Gen key for encryption at rest
-    if ! jobico::was_done "encatrest"; then
+    if [ $(jobico::was_done "encatrest") == false ]; then
         jobico::encryption::gen_key
         NOT_DRY_RUN jobico::encryption::deploy
         jobico::set_done "encatrest"
     fi
     # Etcd
-    if ! jobico::was_done "etcddb"; then
+    if [ $(jobico::was_done "etcddb") == false ]; then
         jobico::etcd::gen_service
         NOT_DRY_RUN jobico::etcd::deploy
         jobico::set_done "etcddb"
     fi
     # Control plane components
-    if ! jobico::was_done "cpl"; then
+    if [ $(jobico::was_done "cpl") == false ]; then
         jobico::cpl::gen_kubeapiserver_service
         jobico::set_done "cpl"
     fi
     # Control Plane deployment
-    if ! jobico::was_done "deploy_server"; then
+    if [ $(jobico::was_done "deploy_server") == false ]; then
         NOT_DRY_RUN jobico::cluster::deploy_to_servers
         jobico::set_done "deploy_server"
     fi
     # Nodes deployment
-    if ! jobico::was_done "deploy_nodes"; then
+    if [ $(jobico::was_done "deploy_nodes") == false ]; then
         NOT_DRY_RUN jobico::cluster::deploy_to_nodes
         jobico::set_done "deploy_nodes"
     fi
     # Routes
-    if ! jobico::was_done "routes"; then
+    if [ $(jobico::was_done "routes") == false ]; then
         NOT_DRY_RUN jobico::net::init
         NOT_DRY_RUN jobico::net::add_routes
         jobico::set_done "routes"
     fi
-    if ! jobico::was_done "lock"; then
+    if [ $(jobico::was_done "lock") == false ]; then
         NOT_DRY_RUN jobico::dao::cluster::lock
         jobico::set_done "lock"
     fi
 }
 jobico::create_nodes() {
-    if ! jobico::was_done "add_machines"; then
+    if [ $(jobico::was_done "add_machines") == false ]; then
         NOT_DRY_RUN jobico::vm::create
         NOT_DRY_RUN jobico::vm::wait_until_all_up
         jobico::set_done "add_machines"
     fi
     # DNS
-    if ! jobico::was_done "add_host"; then
+    if [ $(jobico::was_done "add_host") == false ]; then
         jobico::host::add_new_nodes_to_hostsfile
         NOT_DRY_RUN jobico::host::update_local_etc_hosts
         NOT_DRY_RUN jobico::host::update_local_known_hosts
@@ -101,51 +101,51 @@ jobico::create_nodes() {
         jobico::set_done "add_host"
     fi
     # TLS
-    if ! jobico::was_done "add_tls"; then
+    if [ $(jobico::was_done "add_tls") == false ]; then
         jobico::tls::add_nodes_to_ca_conf
         jobico::tls::gen_certs
         NOT_DRY_RUN jobico::tls::deploy_to_nodes
         jobico::set_done "add_tls"
     fi
     # Kubeconfig
-    if ! jobico::was_done "add_kubeconfig"; then
+    if [ $(jobico::was_done "add_kubeconfig") == false ]; then
         jobico::kubeconfig::gen_for_nodes
         NOT_DRY_RUN jobico::kubeconfig::deploy_to_nodes
         jobico::set_done "add_kubeconfig"
     fi
     # Deployment
-    if ! jobico::was_done "add_deploy_nodes"; then
+    if [ $(jobico::was_done "add_deploy_nodes") == false ]; then
         NOT_DRY_RUN jobico::cluster::deploy_to_nodes
         jobico::set_done "add_deploy_nodes"
     fi
     # Routes
-    if ! jobico::was_done "add_routes"; then
+    if [ $(jobico::was_done "add_routes") == false ]; then
         NOT_DRY_RUN jobico::net::init
         #NOT_DRY_RUN jobico::net::add_routes
         NOT_DRY_RUN jobico::net::add_routes_to_added_node
         jobico::set_done "add_routes"
     fi
     # Merge DBs
-    if ! jobico::was_done "add_merge_db"; then
+    if [ $(jobico::was_done "add_merge_db") == false ]; then
         NOT_DRY_RUN jobico::dao::merge_dbs
         jobico::set_done "add_merge_db"
     fi
     # Lock
-    if ! jobico::was_done "add_lock"; then
+    if [ $(jobico::was_done "add_lock") == false ]; then
         NOT_DRY_RUN jobico::dao::cluster::lock
         jobico::set_done "add_lock"
     fi
 }
 jobico::init_for_add() {
     local number_of_nodes=$1
-    if ! jobico::was_done "add_init"; then
+    if [ $(jobico::was_done "add_init") == false ]; then
         jobico::dao::gen_add_db ${number_of_nodes}
         jobico::dao::gen_add_cluster_db
         jobico::set_done "add_init"
     fi
 }
 jobico::local() {
-    if ! jobico::was_done "locals"; then
+    if [ $(jobico::was_done "locals") == false ]; then
         jobico::local::download_local_deps
         jobico::local::install_kubectl
         jobico::kubeconfig::gen_for_kube_admin
@@ -161,7 +161,7 @@ jobico::restore_local_env() {
 jobico::install_all_addons() {
     local action=$1
     local base_dir=$2
-    if ! jobico::was_done $action; then
+    if [ $(jobico::was_done $action) == false ]; then
         jobico::install_addons $base_dir/core
         jobico::install_addons $base_dir/extras
         jobico::set_done $action
