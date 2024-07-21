6cecddb6aa05ddd8058d11684e931554411bffe6
Andres <andres.costa@gmail.com>
2024-07-21 17:31:57 -0400

    fixes

--- post/extras/jobicok8s/main.sh
+++ post/extras/jobicok8s/main.sh
@@ -3,7 +3,9 @@ if [[ $1 != "" && -d $1 ]]; then
 else
     temp_dir=$(mktemp -d)
 fi
-git clone git@github.com:/andrescosta/jobicok8s.git $temp_dir/jobicok8s
+if [[ ! -d "$1/jobicok8s" ]]; then
+    git clone git@github.com:/andrescosta/jobicok8s.git $temp_dir/jobicok8s
+fi
 cd $temp_dir/jobicok8s
 echo "Installing from: $(pwd)"
 docker login --username jobico --password jobico123 https://reg.jobico.org
