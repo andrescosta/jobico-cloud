2547a7b324e7a94bd00aed8dfe179461e47cf156
Andres <andres.costa@gmail.com>
2024-08-01 10:29:02 -0400

    obs

--- addons/extras/obs/main.sh
+++ addons/extras/obs/main.sh
@@ -3,3 +3,9 @@ helm install kube-prometheus-stack \
   --namespace obs \
   -f $1/values.yaml \
   prometheus-community/kube-prometheus-stack
+
+
+helm install prometheus-postgres-exporter \
+ prometheus-community/prometheus-postgres-exporter -nobs -f $1/values-pg.yaml
+
+kubectl apply -f $1/monitor.yaml
