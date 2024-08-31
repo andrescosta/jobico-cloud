760cae51af88921798476afc6a24dae78accde61
builder <builder@git>
2024-08-30 20:03:15 -0400

    tekton

---
+++ services/extras/tektoncd/main.sh
@@ -0,0 +1,13 @@
+readonly DIR=$2
+. constants.sh
+. ${SCRIPTS}/support/k8s.sh
+
+kubectl apply -f https://storage.googleapis.com/tekton-releases/operator/latest/release.yaml
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/operator/main/config/crs/kubernetes/config/all/operator_v1alpha1_config_cr.yaml
+wait_for_namespace "tekton-pipelines" 60 2
+if [ $? -ne 0 ]; then
+  exit 1
+else
+  copy_secret jobico.org-secret default tekton-pipelines
+  kubectl apply -f $1/ingress.yaml -n tekton-pipelines
+fi
