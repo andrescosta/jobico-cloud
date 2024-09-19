8e90363c1216dc3cdd15ad6765908f5f985b6e43
Andres <andrescosta@users.noreply.github.com>
2024-09-19 10:17:56 -0400

    Uri (#25)
    * changed the domain to jobico.local.
    * new option to specify the domain name.

--- addons/core/dns/main.sh
+++ addons/core/dns/main.sh
@@ -1,2 +1,16 @@
-kubectl apply -f $1/coredns.yaml
-kubectl apply -f $1/excoredns.yaml
+readonly DIR=$2
+. constants.sh
+. ${SCRIPTS}/support/dirs.sh
+load_dirs
+. ${SCRIPTS}/support/utils.sh
+. ${SCRIPTS}/dao/cpl.sh
+
+install(){
+    local domain=$(jobico::dao::cpl::get_domain)
+    local coredns=$(prepare_file "$1/coredns.yaml.tmpl" "dns" "{DOMAIN}=$domain")
+    local excoredns=$(prepare_file "$1/excoredns.yaml.tmpl" "dns" "{DOMAIN}=$domain")
+    kubectl apply -f $coredns
+    kubectl apply -f $excoredns
+}
+
+install "$@"
--- addons/extras/obs/main.sh
+++ addons/extras/obs/main.sh
@@ -1,28 +1,42 @@
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
+readonly DIR=$2
+. constants.sh
+. ${SCRIPTS}/support/dirs.sh
+load_dirs
+. ${SCRIPTS}/support/utils.sh
+. ${SCRIPTS}/dao/cpl.sh
 
-helm install --create-namespace \
-  --values $1/values-loki.yaml \
-  loki \
-  --namespace=obs \
-  grafana/loki
+install(){
+    local domain=$(jobico::dao::cpl::get_domain)
+    local values=$(prepare_file "$1/values.yaml.tmpl" "obs" "{DOMAIN}=$domain")
 
-helm install tempo grafana/tempo --namespace=obs
+    helm repo add grafana https://grafana.github.io/helm-charts
+    helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+    helm repo update
 
-helm install pyroscope grafana/pyroscope --namespace=obs
+    helm install --create-namespace \
+      --values $1/values-loki.yaml \
+      loki \
+      --namespace=obs \
+      grafana/loki
 
-helm install kube-prometheus-stack \
-  --create-namespace \
-  --namespace obs \
-  -f $1/values.yaml \
-  prometheus-community/kube-prometheus-stack
+    helm install tempo grafana/tempo --namespace=obs
+
+    helm install pyroscope grafana/pyroscope --namespace=obs
+
+    helm install kube-prometheus-stack \
+      --create-namespace \
+      --namespace obs \
+      -f $values \
+      prometheus-community/kube-prometheus-stack
 
 
-helm install prometheus-postgres-exporter \
- prometheus-community/prometheus-postgres-exporter -nobs -f $1/values-pg.yaml
+    helm install prometheus-postgres-exporter \
+    prometheus-community/prometheus-postgres-exporter -nobs -f $1/values-pg.yaml
 
-kubectl apply -nobs -f $1/dashboards-jvm-micrometer.yaml
-kubectl apply -nobs -f $1/dashboards-trace.yaml
-kubectl apply -nobs -f $1/dashboards-pg.yaml
-kubectl apply -nobs -f $1/monitor.yaml
+    kubectl apply -nobs -f $1/dashboards-jvm-micrometer.yaml
+    kubectl apply -nobs -f $1/dashboards-trace.yaml
+    kubectl apply -nobs -f $1/dashboards-pg.yaml
+    kubectl apply -nobs -f $1/monitor.yaml
+}
+
+install "$@"
