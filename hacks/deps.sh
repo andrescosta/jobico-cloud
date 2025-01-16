b8849b807d1f4f65d4ec6367daf32d0fd7769e15
andres <andres.costa@gmail.com>
2025-01-16 11:39:09 -0500

    fixed bugs. added latest version

--- hacks/deps.sh
+++ hacks/deps.sh
@@ -3,7 +3,7 @@ sudo apt install -y qemu-kvm virt-manager libvirt-daemon-system virtinst libvirt
 sudo apt install cloud-utils whois -y
 sudo systemctl enable --now libvirtd
 sudo systemctl start libvirtd
-sudo usermod -aG kvm $USER
-sudo usermod -aG libvirt $USER
+sudo usermod -aG libvirt,kvm $USER
 sudo apt install -y dnsmasq-utils
 sudo apt install -y make
+echo "The dependencies were installed. Please logout or restart your computer to refresh the changes."
