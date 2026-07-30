f9e7a425619d93ea5ffec4e40f1dcdabd153e9c1
andres <andrescosta@users.noreply.github.com>
2026-07-30 11:34:26 -0400

    fixes

---
+++ addons/extras/registry/newpod.sh
@@ -0,0 +1,14 @@
+readonly DIR=$2
+. constants.sh
+. ${SCRIPTS}/support/dirs.sh
+load_dirs
+. ${SCRIPTS}/dao/dao.sh
+. ${SCRIPTS}/dao/cluster.sh
+. ${SCRIPTS}/dao/cpl.sh
+. ${SCRIPTS}/support/ssh.sh
+. ${SCRIPTS}/support/utils.sh
+. $1/lib.sh
+install(){
+    manifests "$@"
+}
+install "$@"
--- services/extras/identity/delete.sh
+++ services/extras/identity/delete.sh
@@ -1,9 +1,11 @@
 helm uninstall my-zitadel
 for resource in job configmap secret rolebinding role serviceaccount; do
   kubectl delete $resource --selector app.kubernetes.io/name=zitadel
+  kubectl delete $resource --selector app.kubernetes.io/name=zitadel-login
 done
 
 kubectl get all,secret,configmap,job -l app.kubernetes.io/name=zitadel
+kubectl get all,secret,configmap,job -l app.kubernetes.io/name=zitadel-login
 
 psql "postgresql://postgres:postgres@db.jobico.local:5432/postgres?sslmode=require" -c "DROP DATABASE zitadel WITH (FORCE);"
 
